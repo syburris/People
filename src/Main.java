14787fe7268f32ea666398764479db5ce09833fe
Steven Burris <stevenburris.jr@gmail.com>
2016-09-26 14:19:25 -0400

    Imported People.txt file, created Person Class, created PersonWrapper class, customized ToString method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import jodd.json.JsonSerializer;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    static final String PEOPLE = "People.txt";
+    public static HashMap<String, ArrayList<Person>> peopleMap = new HashMap<>();
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Person> people = new ArrayList<>();
+        File file = new File(PEOPLE);
+        Scanner fileScanner = null;
+        fileScanner = new Scanner(file);
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            String[] columns = line.split("\\,");
+            Person person = new Person(Integer.valueOf(columns[0]),columns[1],columns[2],columns[3],columns[4],columns[5]);
+            people.add(person);
+        }
+        System.out.println(people.toString());
+
     }
 }
---
+++ src/PersonWrapper.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * Created by stevenburris on 9/26/16.
+ */
+public class PersonWrapper {
+
+    public ArrayList<Person> people;
+
+    public PersonWrapper() {
+
+    }
+
+    public ArrayList<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(ArrayList<Person> people) {
+        this.people = people;
+    }
+}
