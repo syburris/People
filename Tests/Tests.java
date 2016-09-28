ef1246fe1f8ba0bf88b326d6351620c14f1a9781
Steven Burris <stevenburris.jr@gmail.com>
2016-09-28 14:16:18 -0400

    finished writing test methods

--- Tests/Tests.java
+++ Tests/Tests.java
@@ -1,7 +1,9 @@
+
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -35,11 +37,33 @@ public class Tests {
     }
 
     @Test
-    public void testWriteJson() {
+    public void testWriteJson() throws IOException {
         // Given
         HashMap<String, ArrayList<Person>> peopleMap = new HashMap<>();
+        ArrayList<Person> people = Main.readFile();
+        Main.addToPeopleMap(people,peopleMap);
+        File file = new File("people.json");
 
+        // If
+        Main.writeJson(peopleMap);
+
+        // Then
+        assertTrue(file.exists());
     }
 
+    @Test
+    public void testSortPeople() throws FileNotFoundException {
+        // Given
+        HashMap<String, ArrayList<Person>> peopleMap = new HashMap<>();
+        ArrayList<Person> people = Main.readFile();
+        Main.addToPeopleMap(people, peopleMap);
 
+
+        // If
+        Main.sortPeople(peopleMap);
+
+        // Then
+        assertTrue(peopleMap.get("China").get(1).lastName.compareTo(peopleMap.get("China").get(2).lastName) < 0);
+
+    }
 }
